d2e03720f929563cd507c3eb3f0d84161d00c369
Your Name <you@example.com>
2024-03-27 15:05:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express=require("express");
+const dotenv=require("dotenv");
+dotenv.config();
+const server=express();
+// server.use(express.json())
+const mongoose=require("mongoose");
+const router=require("./Router/Router");
+const path = require("path");
+const Router=require("./Router/CreatePost");
+const userRouter=require("./Router/UserDetailsRouter");
+const cors = require("cors");
+ server.use(express.json());
+server.use(cors());
+
+const PORT=process.env.PORT || 5000
+// saving the frontend code in the server
+
+
+mongoose.connect(process.env.URL)
+.then((res)=>{
+    console.log("database connected")
+    server.listen(PORT,()=>{
+        console.log(`server is running ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+server.use(express.static(path.join(__dirname, "./frontend/build")));
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html")),
+  function (err) {
+    return res.status(500).send(err)
+  }
+});
+server.use(Router);
+server.use(router);
+server.use(userRouter)
